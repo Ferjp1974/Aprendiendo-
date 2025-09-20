7a9e9a0b6684ef8a8605996f49a49ad181ee676e
fer <tormen_fer@hotmail.com>
2025-09-19 23:06:55 -0300

    me olvide

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Aquí puedes agregar el código JavaScript que deseas ejecutar una vez que el DOM esté completamente cargado
+});
+// Por ejemplo, puedes agregar un evento de clic a un botón
